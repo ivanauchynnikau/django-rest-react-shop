4cd9ea2f340f16b9e269d431d43648f4a678fac1
ivan.auchynnikau <ivan.auchynnikau@itechart-group.com>
2020-09-17 23:37:07 +0300

    delete comments

--- djangoRestReactShop/urls.py
+++ djangoRestReactShop/urls.py
@@ -1,25 +1,3 @@
-"""djangoRestReactShop URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-
-
-
-# base-auth/login
-# auth/register
-# auth/logout
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -28,7 +6,7 @@ from django.conf import settings
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path('admin', admin.site.urls),
     path('api/v1/base-auth/', include('rest_framework.urls')),
-    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.authtoken')),
     path('api/v1/products/', include('product.urls')),
     path('api/v1/categories/', include('category.urls')),
     path('api/v1/orders/', include('order.urls')),
