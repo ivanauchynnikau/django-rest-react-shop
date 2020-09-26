cd2449c11461df093db0b2cc93861604b7db8900
ivan.auchynnikau <ivan.auchynnikau@itechart-group.com>
2020-09-26 21:23:09 +0300

    Add sorting select

--- frontend/src/components/product-list-page/product-list-page.js
+++ frontend/src/components/product-list-page/product-list-page.js
@@ -3,11 +3,18 @@ import {connect} from "react-redux";
 import ProductProvider from "./../../providers/products";
 import Product from "./../../components/product/product";
 import uuid from 'react-uuid'
+import Select from 'react-select';
 
 
 class ProductListPage extends Component {
   static defaultProps = {
     productList: [],
+    selectedSorting: 'default',
+    sortingOptions: [
+      { value: 'default', label: 'Default sorting' },
+      { value: 'lowPrice', label: 'Low price' },
+      { value: 'highPrice', label: 'High price' },
+    ],
   };
 
   constructor(props) {
@@ -18,19 +25,50 @@ class ProductListPage extends Component {
     this.props.productProvider.getProductList();
   }
 
+  onSortingChange = selectedOption => {
+    this.setState({ selectedOption });
+
+    switch(selectedOption) {
+      case 'lowPrice':
+
+        break;
+
+      case 'highPrice':
+
+        break;
+
+      default:
+
+        break;
+    }
+  };
+
   render() {
     const {
-      productList
+      productList,
+      sortingOptions,
+      selectedOption
     } = this.props;
 
     return (
       <div className="product-list-page">
-        {productList.map(product => (
-          <Product
-            key={uuid()}
-            product={product}
-          />
-        ))}
+        <div className="product-list-page__top">
+          <div className="product-list-page__top-sorting">
+            <Select
+              value={selectedOption}
+              onChange={this.onSortingChange}
+              options={sortingOptions}
+            />
+          </div>
+        </div>
+        <div className="product-list-page__content">
+          {productList.map(product => (
+            <Product
+              key={uuid()}
+              product={product}
+            />
+          ))}
+        </div>
       </div>
     );
   }
