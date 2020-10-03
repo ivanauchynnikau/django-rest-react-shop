7a91f05a95aac3ee4ec644b338221e407578a708
ivan.auchynnikau <ivan.auchynnikau@itechart-group.com>
2020-10-03 16:45:05 +0300

    update settings to serve media files on AWS S3

--- djangoRestReactShop/settings.py
+++ djangoRestReactShop/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'storages',
     'product',
     'category',
     'frontend',
@@ -177,3 +178,14 @@ if os.getcwd() == '/app':
 
 
 BASE_WEBSITE_URL = os.getenv('BASE_WEBSITE_URL')
+
+
+# AWS settings
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+DEFAULT_FILE_STORAGE = 'djangoRestReactShop.storage_backends.MediaStorage'
