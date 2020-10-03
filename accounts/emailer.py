aa045cffb6721aad27ce4580917fd065d965dcb1
ivan.auchynnikau <ivan.auchynnikau@itechart-group.com>
2020-10-03 20:43:58 +0300

    Update order/sign-up emails

--- accounts/emailer.py
+++ accounts/emailer.py
@@ -4,12 +4,16 @@ from djangoRestReactShop.settings import EMAIL_HOST_USER, BASE_WEBSITE_URL
 
 def send_email(email, password):
     message = ''
-    message += f'<a href="{BASE_WEBSITE_URL}" target="_blank" style="font-size: 30px;">DJANGOSHOP.COM</a>'
+    message += f'<div style="max-width: 320px;">'
+    message += f'<br>'
+    message += f'<a href="{BASE_WEBSITE_URL}" target="_blank" style="font-size: 15px;">{BASE_WEBSITE_URL}</a>'
+    message += f'<h3>Congratulations! You have successfully registered!</p>'
+    message += f'<br>'
     message += f'<p>Email: { email }</p>'
     message += f'<p>Password: { password }</p>'
-    message += f'<br><hr><br>'
+    message += f'<div/>'
 
-    subject = 'Your account was created.'
+    subject = 'Account was created!'
 
     msg = EmailMultiAlternatives(subject, subject, EMAIL_HOST_USER, [email])
     msg.attach_alternative(message, "text/html")
--- order/emailer.py
+++ order/emailer.py
@@ -1,16 +1,22 @@
 from django.core.mail import EmailMultiAlternatives
-from djangoRestReactShop.settings import EMAIL_HOST_USER, BASE_WEBSITE_URL
+from djangoRestReactShop.settings import EMAIL_HOST_USER, BASE_WEBSITE_URL, AWS_STORAGE_BUCKET_NAME
 
 
 def send_email(recipient_mail, order_id, product_list):
     message = ''
+    message += f'<div style="max-width: 320px;">'
+    message += f'<br>'
+    message += f'<a href="{BASE_WEBSITE_URL}" target="_blank" style="font-size: 15px;">{BASE_WEBSITE_URL}</a>'
+    message += f'<br><hr>'
+
     for product in product_list:
-        message += f'<a href="{BASE_WEBSITE_URL}" target="_blank" style="font-size: 30px;">DJANGOSHOP.COM</a>'
         message += f'<p style="font-size: 22px;">{ product["title"] }</p>'
-        message += f'<div><img src="{ product["image"] }"></div>'
+        message += f'<div><img style="max-width: 240px;" src="https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{ product["image"] }"></div>'
         message += f'<p>Description: { product["description"] }</p>'
         message += f'<p style="font-size: 18px;">Price: { product["price"] }</p>'
         message += f'<br><hr><br>'
+
+    message += f'<div/>'
 
     subject = 'Order №{} was created.'.format(order_id)
 
