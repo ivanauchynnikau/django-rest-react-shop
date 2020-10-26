960ae91930b5a19600959abe747144da056ed551
ivan.auchynnikau <ivan.auchynnikau@itechart-group.com>
2020-10-26 20:51:23 +0300

    admin. add info about order items to order page

--- order/admin.py
+++ order/admin.py
@@ -1,8 +1,11 @@
-from django.contrib import admin
-
 # Register your models here.
 from django.contrib import admin
 from order.models import Order
+from orderItem.models import OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
 
 
 class OrderAdmin(admin.ModelAdmin):
@@ -10,6 +13,9 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ['user', 'state', 'comment']
     list_editable = ['comment']
     list_display_links = ['user', 'state']
+    inlines = [
+        OrderItemInline,
+    ]
 
 
 admin.site.register(Order, OrderAdmin)
