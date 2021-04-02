f9471d0e755c87c1a9630527af575c43122dec0c
ivan.auchynnikau <ivan.auchynnikau@itechart-group.com>
2021-04-02 19:43:11 +0300

    refactor: code style change for emailer.py files

--- accounts/emailer.py
+++ accounts/emailer.py
@@ -3,15 +3,15 @@ from djangoRestReactShop.settings import EMAIL_HOST_USER, BASE_WEBSITE_URL
 
 
 def send_email(email, password):
-    message = ''
-    message += f'<div style="max-width: 320px;">'
-    message += f'<br>'
-    message += f'<a href="{BASE_WEBSITE_URL}" target="_blank" style="font-size: 15px;">{BASE_WEBSITE_URL}</a>'
-    message += f'<h3>Congratulations! You have successfully registered!</p>'
-    message += f'<br>'
-    message += f'<p>Email: { email }</p>'
-    message += f'<p>Password: { password }</p>'
-    message += f'<div/>'
+    message = (
+        f'<div style="max-width: 320px;">{email}'
+        f'<br>'
+        f'<a href="{BASE_WEBSITE_URL}" target="_blank" style="font-size: 15px;">{BASE_WEBSITE_URL}</a>'
+        f'<h3>Congratulations! You have successfully registered!</h3>'
+        f'<br>'
+        f'<p>Email: {email}</p><p>Password: {password}</p>'
+        f'<div/>'
+    )
 
     subject = 'Account was created!'
 
--- order/emailer.py
+++ order/emailer.py
@@ -2,23 +2,31 @@ from django.core.mail import EmailMultiAlternatives
 from djangoRestReactShop.settings import EMAIL_HOST_USER, BASE_WEBSITE_URL, AWS_STORAGE_BUCKET_NAME
 
 
-def send_email(recipient_mail, order_id, product_list):
+def get_product_list_html(product_list):
     message = ''
-    message += f'<div style="max-width: 320px;">'
-    message += f'<br>'
-    message += f'<a href="{BASE_WEBSITE_URL}" target="_blank" style="font-size: 15px;">{BASE_WEBSITE_URL}</a>'
-    message += f'<br><hr>'
-
     for product in product_list:
-        message += f'<p style="font-size: 22px;">{ product["title"] }</p>'
-        message += f'<div><img style="max-width: 320px;" src="https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{ product["image"] }"></div>'
-        message += f'<p>Description: { product["description"] }</p>'
-        message += f'<p style="font-size: 18px;">Price: { product["price"] }</p>'
-        message += f'<br><hr><br>'
+        message += (
+            f'<p style="font-size: 22px;">{ product["title"] }</p>'
+            f'<div><img style="max-width: 320px;" src="https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{ product["image"] }"></div>'
+            f'<p>Description: { product["description"] }</p>'
+            f'<p style="font-size: 18px;">Price: { product["price"] }</p>'
+            f'<br><hr><br>'
+        )
 
-    message += f'<div/>'
+    return message
 
-    subject = 'Order №{} was created.'.format(order_id)
+
+def send_email(recipient_mail, order_id, product_list):
+    message = (
+        f'<div style="max-width: 320px;">'
+        f'<br>'
+        f'<a href="{BASE_WEBSITE_URL}" target="_blank" style="font-size: 15px;">{BASE_WEBSITE_URL}</a>'
+        f'<br><hr>'
+        f'{get_product_list_html(product_list)}'
+        f'<div/>'
+    )
+
+    subject = f'Order №{order_id} was created.'
 
     msg = EmailMultiAlternatives(subject, subject, EMAIL_HOST_USER, [recipient_mail])
     msg.attach_alternative(message, "text/html")
