17332f3f5338645a502b0b256a325c05a586bbf5
ivan.auchynnikau <ivan.auchynnikau@itechart-group.com>
2021-04-02 20:14:16 +0300

    refactor tests_url.py

--- accounts/tests/tests_url.py
+++ accounts/tests/tests_url.py
@@ -4,27 +4,30 @@ from accounts.models import MyUser
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 
-email = 'testuser@example.com'
-password = 'password123'
-
 
 class AccountTests(TestCase):
     def setUp(self):
-        self.user = MyUser.objects.create_user(email=email, password=password)
+        self.email = "testuser1@example.com"
+        self.password = "password123"
+        self.user = MyUser.objects.create_user(email=self.email, password=self.password)
+
         self.token = Token.objects.create(user=self.user).key
         self.client = APIClient()
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
 
     def test_login(self):
-        response = self.client.post(reverse('accounts:user_login'), {'email': email, 'password': password})
+        response = self.client.post(reverse('accounts:user_login'), {'email': self.email, 'password': self.password})
         assert response.status_code == 200
 
     def test_get_user(self):
-        response = self.client.get(reverse('accounts:user_details'), {'email': email, 'password': password})
+        response = self.client.get(reverse('accounts:user_details'), {'email': self.email, 'password': self.password})
         assert response.status_code == 200
 
 
 class AccountSignUpTest(TestCase):
     def test_sign_up(self):
-        response = self.client.post(reverse('accounts:user_sign_up'), {'email': email, 'password': password})
+        self.email = "testuser2@example.com"
+        self.password = "password123"
+
+        response = self.client.post(reverse('accounts:user_sign_up'), {'email': self.email, 'password': self.password})
         assert response.status_code == 201
